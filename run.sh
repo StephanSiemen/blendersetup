14c04baae4adfa49eb0982501ee920e27e2ca374
Stephan Siemen <stephan.siemen@ecmwf.int>
2021-09-02 13:26:34 +0100

    make first example run and doc centos reqs

--- run.sh
+++ run.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-SCRIPT = 'GlobeTexture_withHeight.py'
-FILE   = 'blender-2.93.4-linux-x64.tar.xz'
+#
+# prereq on CentOS: 
+#     sudo yum install libX11 libXi libXrender libXxf86vm libXfixes
+#
 
-LOC = 'blendersetup'
+SCRIPT='GlobeTexture_withHeight.py'
 
-if [ ! -f $FILE]; then
-   echo "Download $FILE ..."
+BL_FILE='blender-2.93.4-linux-x64.tar.xz'
+
+LOC='blendersetup'
+
+if [ ! -f $BL_FILE]; then
+   echo "Download $BL_FILE ..."
    wget 'https://download.blender.org/release/Blender2.93/blender-2.93.4-linux-x64.tar.xz'
-   tar -xf $FILE
+   tar -xf $BL_FILE
 fi
 
 blender-2.93.4-linux-x64/blender -b --python ${LOC}/${SCRIPT} -noaudio -E 'CYCLES' -f 1 -F 'PNG'
- 
