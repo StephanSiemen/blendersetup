f4504345ca3c378b69259a85b04feebe50df2153
Stephan Siemen <stephan.siemen@ecmwf.int>
2021-09-02 13:28:39 +0100

    fix test

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ BL_FILE='blender-2.93.4-linux-x64.tar.xz'
 
 LOC='blendersetup'
 
-if [ ! -f $BL_FILE]; then
+if [ ! -f ${BL_FILE} ]; then
    echo "Download $BL_FILE ..."
    wget 'https://download.blender.org/release/Blender2.93/blender-2.93.4-linux-x64.tar.xz'
    tar -xf $BL_FILE
