ad8b3f40dcd2ec984f0a4aa6d1b7693cd8535796
Stephan Siemen <stephan.siemen@ecmwf.int>
2021-09-15 20:51:57 +0200

    correct run path

--- run.sh
+++ run.sh
@@ -9,12 +9,14 @@ SCRIPT='GlobeTexture_withHeight.py'
 
 BL_FILE='blender-2.93.4-linux-x64.tar.xz'
 
-LOC='blendersetup'
-
 if [ ! -f ${BL_FILE} ]; then
    echo "Download $BL_FILE ..."
    wget 'https://download.blender.org/release/Blender2.93/blender-2.93.4-linux-x64.tar.xz'
    tar -xf $BL_FILE
 fi
 
-blender-2.93.4-linux-x64/blender -b --python ${LOC}/${SCRIPT} -noaudio -E 'CYCLES' -o //render_ -f 1 -F 'PNG'
+blender-2.93.4-linux-x64/blender -b --python ${SCRIPT} -noaudio -E 'CYCLES' -o //render_ -f 1 -F 'PNG'
+
+##### copy output
+#
+#  
