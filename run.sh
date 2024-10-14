3a97f5b3b76d468b1cc24b1f33124d8878f2590c
StephanSiemen <stephan.siemen@ecmwf.int>
2024-10-14 22:51:54 +0100

    Update to Blender version 4.2.2

--- run.sh
+++ run.sh
@@ -7,15 +7,15 @@
 
 SCRIPT='GlobeTexture_withHeight.py'
 
-BL_FILE='blender-2.93.4-linux-x64.tar.xz'
+BL_FILE='blender-4.2.2-linux-x64.tar.xz'
 
 if [ ! -f ${BL_FILE} ]; then
    echo "Download $BL_FILE ..."
-   wget 'https://download.blender.org/release/Blender2.93/blender-2.93.4-linux-x64.tar.xz'
+   wget 'https://download.blender.org/release/Blender4.2/blender-4.2.2-linux-x64.tar.xz'
    tar -xf $BL_FILE
 fi
 
-blender-2.93.4-linux-x64/blender -b --python ${SCRIPT} -noaudio -E 'CYCLES' -o //render_ -f 1 -F 'PNG'
+blender-4.2.2-linux-x64/blender -b --python ${SCRIPT} -noaudio -E 'CYCLES' -o //render_ -f 1 -F 'PNG'
 
 ##### copy output
 #
